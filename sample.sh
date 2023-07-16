1189a97a604898bd502fb54a154df440c17f2321
stalendp <stalendp@gmail.com>
2023-07-16 20:23:12 +1000

    fix message

--- sample.sh
+++ sample.sh
@@ -15,12 +15,15 @@ function sample_BraTS {
         echo "sample step  : $3"
         echo "acceleration : $4"
         echo "show progress: $5"
-
-        python -m scripts.image_sample  --work_dir $ROOT_DIR/working \
+        echo "--------------------------------------------"
+        cmd="python -m scripts.image_sample  --work_dir $ROOT_DIR/working \
                 --model_path $ROOT_DIR/evaluations/BraTS/model_2.02m.pt --testset_path $ROOT_DIR/evaluations/BraTS/BraTS.npz \
                 --attention_resolutions 30 --class_cond False --learn_sigma True --noise_schedule cosine \
                 --image_size 240 --num_channels 32 --num_res_blocks 3  --channel_mult "1,2,2,4,4" --use_ddim True \
-                --num_samples $1 --batch_size $2 --timestep_respacing "ddim$3" --acceleration $4  --show_progress $5
+                --num_samples $1 --batch_size $2 --timestep_respacing "ddim$3" --acceleration $4  --show_progress $5"
+        echo $cmd | tr -s ' '
+        echo "--------------------------------------------"
+        eval $cmd
 }
 
 num=30
