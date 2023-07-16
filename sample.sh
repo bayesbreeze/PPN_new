54b1b6c76bb5993eceb64701318668cb32ddf60b
stalendp <stalendp@gmail.com>
2023-07-16 20:35:27 +1000

    fix command

--- sample.sh
+++ sample.sh
@@ -2,12 +2,13 @@
 #SBATCH --partition=vgpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
+#SBATCH --output=working/sample_%j.out
+#SBATCH --job-name=sample
 
 conda activate adm
 
 ROOT_DIR="/home/Student/s4623598/weijiang/_Miccai/PPN_new"
 cd $ROOT_DIR
-
 
 function sample_BraTS {
         echo "sample number: $1"
@@ -19,8 +20,8 @@ function sample_BraTS {
         cmd="python -m scripts.image_sample  --work_dir $ROOT_DIR/working \
                 --model_path $ROOT_DIR/evaluations/BraTS/model_2.02m.pt --testset_path $ROOT_DIR/evaluations/BraTS/BraTS.npz \
                 --attention_resolutions 30 --class_cond False --learn_sigma True --noise_schedule cosine \
-                --image_size 240 --num_channels 32 --num_res_blocks 3  --channel_mult "1,2,2,4,4" --use_ddim True \
-                --num_samples $1 --batch_size $2 --timestep_respacing "ddim$3" --acceleration $4  --show_progress $5"
+                --image_size 240 --num_channels 32 --num_res_blocks 3  --channel_mult 1,2,2,4,4 --use_ddim True \
+                --num_samples $1 --batch_size $2 --timestep_respacing ddim$3 --acceleration $4  --show_progress $5"
         echo $cmd | tr -s ' '
         echo "--------------------------------------------"
         eval $cmd
