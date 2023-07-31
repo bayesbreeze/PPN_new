7b3242ea19c6235b7f0808c5f4f3811a9c9508ad
bayesbreeze <100142758+bayesbreeze@users.noreply.github.com>
2023-07-31 21:43:35 +1000

    multicoil

--- ppn/ppn_diffusion.py
+++ ppn/ppn_diffusion.py
@@ -32,19 +32,25 @@ class PPN_Diffusion(SpacedDiffusion):
                     'PPN':self.ppn, 'DDPM': self.ddpm, 'DDIM':self.ddim,
                     'real':self.ppn, 'complex': self.ppn_complex, 'multicoil':self.ppn_multicoil
                 } [sampleType]
+            
+        self.isComplex = sampleType in ['complex', 'multicoil']
         
         print("Sampling type: ", sampleType)
 
         if sampleType == "multicoil":
-            sens = sens.to(device)
-            mixstepsize = int(self.num_timesteps * mixpercent) 
+            self.sens = sens.to(device)
+            self.mixstepsize = int(self.num_timesteps * mixpercent) 
         else:
-            sens = None
-            mixstepsize = 0
+            self.sens = None
+            self.mixstepsize = 0
 
-        mask = mask.to(device)
-        knowns = kspaces.to(device) * mask
-        x = th.randn_like(knowns.real, device=device)
+        self.mask = mask.to(device)
+        self.knowns = kspaces.to(device) * mask
+        x = th.randn_like(self.knowns.real, device=device)
+        # if self.isComplex:
+        #     x = th.randn_like(self.knowns, device=device)
+        # else:
+            
     
         _indices = list(range(self.num_timesteps))[::-1]
         if progress:
@@ -55,30 +61,40 @@ class PPN_Diffusion(SpacedDiffusion):
             x = sample_fn (
                 model,
                 x,
-                i,
-                mask,
-                knowns,
-                sens,
-                mixstepsize
+                i
             ) 
         return x, self.num_timesteps
 
-    def ppn_multicoil(self, model, x, t, mask, known, sens, mixstepsize):
+    def A(self, x):
+        return self.mask * fft2_m(self.sens * x)
+
+    def A_H(self, x):  # Hermitian transpose
+        return torch.sum(torch.conj(self.sens) * ifft2_m(x * self.mask), dim=1).unsqueeze(dim=1)
+
+    def kaczmarz(self, x, y,lamb=1.0): #[1, 15, 320, 320])
+        x = x + lamb * self.A_H(y - self.A(x)) # [1, 15, 320, 320]) + [1, 1, 320, 320])
+        return x
+
+    def ppn_multicoil(self, model, x, t):
         # multiple condition
-        
+        x = self.ppn(model, x, t)
         # combine condition
+        if t % self.mixstepsize == 0:
+            lamb = 1.0 # lamb_schedule.get_current_lambda(t)
+            x = self.kaczmarz(x, self.knowns, self.sens, self.mask, lamb=lamb)
+
+        return x
+
+    def ppn_complex(self, model, x, t):
         pass
 
-    def ppn_complex(self, model, x, t, mask, known, sens=None, mixstepsize=0):
-        pass
-
-    def ppn(self, model, x, t, mask, known, sens=None, mixstepsize=0): # 0: x_0, 1: x_t
+    def ppn(self, model, x, t): # 0: x_0, 1: x_t
 
         ts = th.tensor([t]  * x.shape[0], device=x.device)
 
         def projector(x):
             x_space = to_space(x)
-            x_space = merge_known_with_mask(x_space, known, mask)
+            x_space = merge_known_with_mask(x_space, self.knowns, self.mask)
             return from_space(x_space)
 
         return self._ppn_sample(model, x, ts, projector=projector)
@@ -90,14 +106,14 @@ class PPN_Diffusion(SpacedDiffusion):
         return x
 
     def noisor(self, x_0_hat, t, x_0):
-        return th.sqrt(alpha_bar_prev) * x_0_hat + th.sqrt(1-alpha_bar_prev)* th.randn_like(x_0_hat)
+        return x_0_hat #th.sqrt(alpha_bar_prev) * x_0_hat + th.sqrt(1-alpha_bar_prev)* th.randn_like(x_0_hat)
 
 
-    def ddpm(self, model, x, t, mask, known, sens=None, mixstepsize=0):
+    def ddpm(self, model, x, t):
         ts = th.tensor([t]  * x.shape[0], device=x.device)
         return self.p_sample(model, x, ts)['sample']
     
-    def ddim(self, model, x, t, mask, known, sens=None, mixstepsize=0):
+    def ddim(self, model, x, t):
         ts = th.tensor([t]  * x.shape[0], device=x.device)
         return self.ddim_sample(model, x, ts)['sample']
 
